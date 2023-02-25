e4395b726533bf7a8d5f3dada90abd438ced9fc8
vitalikys <vitalikys87@gmail.com>
2023-02-26 00:01:15 +0200

    add X-Process-Time-header middleware

--- app_short_redis/main.py
+++ app_short_redis/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 
 from app_short_redis.routers import router
 from app_short_redis.router_html import router_template
@@ -9,6 +11,16 @@ app = FastAPI(
     version='0.9.99',
     contact={'name': 'Vitalii Kostyreva', 'tel': '0737776107'}
 )
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
 
 app.include_router(router, tags=["Url API core"])
 app.include_router(router_template, tags=['html_template'])  # another router, for tml templates
